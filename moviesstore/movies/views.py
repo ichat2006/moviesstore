b9623783c6cdeeb805944e973bfd08b42d093f59
Ishaan Chaturvedi <ichaturvedi8@gatech.edu>
2026-09-12 22:18:42 -0400

    Add reporting

--- moviesstore/movies/views.py
+++ moviesstore/movies/views.py
@@ -1,67 +1,123 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Movie, Review
 from django.contrib.auth.decorators import login_required
+
+from .models import Movie, Review
+
 
 def index(request):
     search_term = request.GET.get('search')
+
     if search_term:
-        movies =Movie.objects.filter(name__icontains=search_term)
+        movies = Movie.objects.filter(name__icontains=search_term)
     else:
         movies = Movie.objects.all()
-    template_data = {}
-    template_data['title'] = 'Movies'
-    template_data['movies'] = Movie.objects.all()
-    return render(request, 'movies/index.html',
-                  {'template_data': template_data})
+
+    template_data = {
+        'title': 'Movies',
+        'movies': movies,
+    }
+
+    return render(
+        request,
+        'movies/index.html',
+        {'template_data': template_data}
+    )
+
+
 def show(request, id):
-    movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie, reported=False)
-    template_data = {}
-    template_data['title'] = movie.name
-    template_data['movie'] = movie
-    template_data['reviews'] = reviews
-    return render(request, 'movies/show.html',
-                  {'template_data': template_data})
-# Create your views here.
+    movie = get_object_or_404(Movie, id=id)
+
+    reviews = Review.objects.filter(
+        movie=movie,
+        reported=False
+    )
+
+    template_data = {
+        'title': movie.name,
+        'movie': movie,
+        'reviews': reviews,
+    }
+
+    return render(
+        request,
+        'movies/show.html',
+        {'template_data': template_data}
+    )
+
+
 @login_required
 def create_review(request, id):
-    if request.method == 'POST' and request.POST['comment'] != '':
-        movie = Movie.objects.get(id=id)
-        review = Review()
-        review.comment = request.POST['comment']
-        review.movie = movie
-        review.user = request.user
-        review.save()
-        return redirect('movies.show', id=id)
-    else:
-        return redirect('movies.show', id=id)
+    movie = get_object_or_404(Movie, id=id)
+
+    if request.method == 'POST':
+        comment = request.POST.get('comment', '').strip()
+
+        if comment:
+            Review.objects.create(
+                comment=comment,
+                movie=movie,
+                user=request.user
+            )
+
+    return redirect('movies.show', id=id)
+
+
 @login_required
 def edit_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id)
+    review = get_object_or_404(
+        Review,
+        id=review_id,
+        movie_id=id
+    )
+
     if request.user != review.user:
         return redirect('movies.show', id=id)
-    if request.method == 'GET':
-        template_data = {}
-        template_data['title'] = 'Edit Review'
-        template_data['review'] = review
-        return render(request, 'movies/edit_review.html',
-            {'template_data': template_data})
-    elif request.method == 'POST' and request.POST['comment'] != '':
-        review = Review.objects.get(id=review_id)
-        review.comment = request.POST['comment']
-        review.save()
-        return redirect('movies.show', id=id)
-    else:
-        return redirect('movies.show', id=id)
+
+    if request.method == 'POST':
+        comment = request.POST.get('comment', '').strip()
+
+        if comment:
+            review.comment = comment
+            review.save()
+
+            return redirect('movies.show', id=id)
+
+    template_data = {
+        'title': 'Edit Review',
+        'review': review,
+    }
+
+    return render(
+        request,
+        'movies/edit_review.html',
+        {'template_data': template_data}
+    )
+
+
 @login_required
 def delete_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id,
-        user=request.user)
+    review = get_object_or_404(
+        Review,
+        id=review_id,
+        movie_id=id,
+        user=request.user
+    )
+
     review.delete()
+
     return redirect('movies.show', id=id)
+
+
 @login_required
 def report_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id, movie_id = id)
-    review.reported= True
-    review.save()
+    review = get_object_or_404(
+        Review,
+        id=review_id,
+        movie_id=id
+    )
+
+    if review.user != request.user:
+        review.reported = True
+        review.save()
+
     return redirect('movies.show', id=id)
